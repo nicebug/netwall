457e1d8220260ab4d6de8add0775d404479acf4e
alldoisbug <alldoisbug@71fcdde4-919d-18e9-f485-6d293d36599a>
2012-05-09 08:11:29 +0000

    更改版本号到1.1

--- trunk/src/org/nice/droidwall/Api.java
+++ trunk/src/org/nice/droidwall/Api.java
@@ -44,7 +44,7 @@ import android.widget.Toast;
  */
 public final class Api {
 	/** application version string */
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	/** special application UID used to indicate "any application" */
 	public static final int SPECIAL_UID_ANY	= -10;
 	/** special application UID used to indicate the Linux Kernel */
